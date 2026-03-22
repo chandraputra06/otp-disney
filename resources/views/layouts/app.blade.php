836987c32c22dda0100e1e7385c9a8a6ec1b5d29
chandraputra06 <chandra.wisesa06@gmail.com>
2026-03-22 14:52:59 +0800

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Disney OTP Orinimo</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('build/assets/app-CdcrNCuT.css') }}">
+    <script type="module" src="{{ asset('build/assets/app-CiZEzA8M.js') }}"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
      <link rel="shortcut icon" href="{{ asset('asset/logo-orinimo.png') }}">
 </head>
